1c9e7fdc52a5ccb8988e313c48db05a491e861b0
alfinpradana99 <alfinpradana99@gmail.com>
2019-10-30 20:30:45 +0700

    final

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (function() {
- 
   var canvas = document.getElementById("glcanvas");
   var gl = glUtils.checkWebGL(canvas);
   var program,program2,program3, program4;
@@ -16,126 +15,55 @@
       // Initialize the shaders and program
       var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex),
           vertexShader2 = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v2.vertex),
+          vertexShader3 = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v3.vertex), // for the cube
           fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
-  
-      program = glUtils.createProgram(gl, vertexShader, fragmentShader);
-      program2 = glUtils.createProgram(gl, vertexShader2, fragmentShader);
  
-      // untuk huruf pertama
-      var thetaLoc = gl.getUniformLocation(program, 'theta');
-      var theta = 0;
-      var thetaLoc1 = gl.getUniformLocation(program2, 'theta1');
-      var theta1 = 0;
-    
-      // untuk huruf kedua
-      var scaleLoc = gl.getUniformLocation(program3, 'scale');
-      var scale = 1;
-      var membesar = 1;
-      var scaleLoc1 = gl.getUniformLocation(program4, 'scale1');
-      var scale1 = 1;
-      var membesar1 = 1;
+      //untuk huruf A
+      program = glUtils.createProgram(gl, vertexShader, fragmentShader);
+      var thetaLoc = gl.getUniformLocation(program, 'theta'); 
+      var transLoc = gl.getUniformLocation(program, 'trans');
+      var thetaA = [10, 20, 0];
+      var trans = [0, 0, 0]; 
+      var X = 0.0080;
+      var Y = 0.0090;
+      var Z = 0.0130;
+
+      program2 = glUtils.createProgram(gl, vertexShader2, fragmentShader);
+      var thetaLoc1 = gl.getUniformLocation(program2, 'theta1'); 
+      var transLoc1 = gl.getUniformLocation(program2, 'trans1');
+      var thetaA1 = [10, 20, 0];
+      var trans1 = [0, 0, 0]; 
+      var X1 = 0.0080;
+      var Y1 = 0.0090;
+      var Z1 = 0.0130;
+
+      // untuk cubenya
+      program3 = glUtils.createProgram(gl, vertexShader3, fragmentShader);
+      var thetaLocCube = gl.getUniformLocation(program3, 'theta');
+      var thetaCube;
 
       function render(){
         gl.clearColor(0, 0, 0, 1);
         gl.colorMask(true,true,true,true);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        // untuk merender huruf pertama
         gl.useProgram(program);
-        drawline();
-        theta += 0.0191;
-        gl.uniform1f(thetaLoc, theta);
-        gl.drawArrays(gl.LINE_LOOP,0,8);
+        drawtriangle();
+        gl.drawArrays(gl.TRIANGLE_FAN,0,9);
 
         gl.useProgram(program2);
-        drawline2();
-        theta1 += 0.0191;
-        gl.uniform1f(thetaLoc1, theta1);
-        gl.drawArrays(gl.LINE_LOOP,0,4);
-
-        // untuk merender huruf kedua
-        gl.useProgram(program3);
-        drawtriangle();
-        if(scale >=1) membesar = -1;
-        else if (scale <= -1) membesar = 1;
-        scale = scale + (membesar * 0.0191);
-        gl.uniform1f(scaleLoc,scale);
-        gl.drawArrays(gl.TRIANGLE_STRIP,0,9);
-
-        gl.useProgram(program4);
         drawtriangle2();
-        if(scale1 >=1) membesar1 = -1;
-        else if (scale1 <= -1) membesar1 = 1;
-        scale1 = scale1 + (membesar1 * 0.0191);
-        gl.uniform1f(scaleLoc1,scale1);
         gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
 
+        gl.useProgram(program3);
+        thetaCube = [10, 10, 0];
+        gl.uniform3fv(thetaLocCube, thetaCube);
+
         drawcube();
+
+        gl.drawArrays(gl.LINES,0,24);
         
         requestAnimationFrame(render);
-      }
-
-      function drawline(){
-        var lineVertices = [
-          -0.8, -0.3, 1.0,1.0,0.0,
-          -0.5, +0.6, 0.2,0.2,1.0,
-          -0.45, +0.6, 0.6,1.0,0.6,
-          -0.15, -0.3, 0.1,0.7,1.0,
-          -0.25, -0.3, 0.7,0.1,1.0,
-          -0.35, 0.0, 1.0,0.0,0.5,
-          -0.60, 0.0, 0.7,0.5,1.0,
-          -0.7, -0.3, 0.4, 0.5, 1.0,
-        ];
-        var lineVertexBufferObject = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, lineVertexBufferObject);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lineVertices), gl.STATIC_DRAW);
-
-        var vPosition = gl.getAttribLocation(program,'vPosition');
-        var vColor = gl.getAttribLocation(program,'vColor');
-        gl.vertexAttribPointer(
-          vPosition,                         
-          2,                                  // jumlah elemen per attribute
-          gl.FLOAT,                           // tipe data attribut
-          gl.FALSE,                           // default
-          5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex
-          0                                   // offset dari posisi elemen di array
-        );
-        gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE,
-          5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-
-        gl.enableVertexAttribArray(vPosition);
-        gl.enableVertexAttribArray(vColor);
-      }
-
-      function drawline2(){
-      
-        var lineVertices2 = [
-          -0.6, +0.1, 1.0,1.0,0.0,
-          -0.35, +0.1, 0.2,0.2,1.0,
-          -0.45, +0.45, 0.1,0.5,0.6,
-          -0.50, 0.45, 0.2,0.2,1.0
-        ];
-
-        var lineVertexBufferObject = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, lineVertexBufferObject);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lineVertices2), gl.STATIC_DRAW);
-
-        var vPosition = gl.getAttribLocation(program,'vPosition');
-        var vColor = gl.getAttribLocation(program,'vColor');
-        gl.vertexAttribPointer(
-          vPosition,                         
-          2,                                  // jumlah elemen per attribute
-          gl.FLOAT,                           // tipe data attribut
-          gl.FALSE,                           // default
-          5 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap vertex
-          0                                   // offset dari posisi elemen di array
-        );
-        gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE,
-          5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-
-        gl.enableVertexAttribArray(vPosition);
-        gl.enableVertexAttribArray(vColor);
-
       }
 
       function drawcube(){
@@ -202,14 +130,14 @@
           0.45, 0.4, 0.2,0.0,1.0,
           0.7, -0.3, 1.0,1.0,0.0,
           0.6, -0.3, 0.4,0.0,1.0
-
         ];
+
         var triangleVertexBufferObject = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
 
-        var vPosition = gl.getAttribLocation(program3,'vPosition');
-        var vColor = gl.getAttribLocation(program3,'vColor');
+        var vPosition = gl.getAttribLocation(program,'vPosition');
+        var vColor = gl.getAttribLocation(program,'vColor');
         gl.vertexAttribPointer(
           vPosition,                          
           2,                                  // jumlah elemen per attribute
@@ -220,6 +148,27 @@
         );
         gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE,
           5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+
+          if(trans[0] >= 0.4*0.8 || trans[0] <= -0.3*0.8 ){
+            X *= -1;
+          }
+          trans[0] += X;
+    
+          if(trans[1] >= 0.6*0.8 || trans[1] <= -0.8*0.8 ){
+            Y *= -1;
+          }
+          trans[1] += Y;
+    
+          if(trans[2] >= 0.7*0.8 || trans[2] <= -0.6*0.8){
+            Z *= -1;
+          }
+          trans[2] += Z;
+    
+          gl.uniform3fv(transLoc, trans);
+
+
+        thetaA[1] += 0.191;
+        gl.uniform3fv(thetaLoc, thetaA);
 
         gl.enableVertexAttribArray(vPosition);
         gl.enableVertexAttribArray(vColor);
@@ -236,8 +185,8 @@
         gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices2), gl.STATIC_DRAW);
 
-        var vPosition = gl.getAttribLocation(program3,'vPosition');
-        var vColor = gl.getAttribLocation(program3,'vColor');
+        var vPosition = gl.getAttribLocation(program2,'vPosition');
+        var vColor = gl.getAttribLocation(program2,'vColor');
         gl.vertexAttribPointer(
           vPosition,                       
           2,                                  // jumlah elemen per attribute
@@ -248,6 +197,27 @@
         );
         gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE,
           5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+          
+          if(trans1[0] >= 0.4*0.8 || trans1[0] <= -0.3*0.8 ){
+            X1 *= -1;
+          }
+          trans1[0] += X1;
+    
+          if(trans1[1] >= 0.6*0.8 || trans1[1] <= -0.8*0.8 ){
+            Y1 *= -1;
+          }
+          trans1[1] += Y1;
+    
+          if(trans1[2] >= 0.7*0.8 || trans1[2] <= -0.6*0.8){
+            Z1 *= -1;
+          }
+          trans1[2] += Z1;
+    
+          gl.uniform3fv(transLoc1, trans1);
+
+
+        thetaA1[1] += 0.191;
+        gl.uniform3fv(thetaLoc1, thetaA1);
 
         gl.enableVertexAttribArray(vPosition);
         gl.enableVertexAttribArray(vColor);
