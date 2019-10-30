1b1cf7d2053efecd51e41c95aba3212930b31c5c
alfinpradana99 <alfinpradana99@gmail.com>
2019-10-30 18:25:18 +0700

    menghilangkan huruf di kiri tugas 2

--- vertex.glsl
+++ vertex.glsl
@@ -1,17 +1,38 @@
+precision mediump float;
 attribute vec2 vPosition;
 attribute vec3 vColor;
 varying vec3 fColor;
-uniform float theta;
+uniform vec3 theta;
+uniform vec3 trans;
+
 void main() {
   fColor = vColor;
-  mat4 rotasi = mat4(
-    cos(theta), -sin(theta), 0.0, +0.5*cos(theta) -0.5,
-    sin(theta), cos(theta), 0.0, +0.5*sin(theta),
+  vec3 angle = radians(theta);
+  vec3 s = sin(angle);
+  vec3 c = cos(angle);
+ 
+
+  mat4 ry = mat4(
+    c.y, 0.0, -s.y, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    s.y, 0.0, c.y, 0.0,
+    0.0, 0.0, 0.0, 1.0
+  );
+   
+  mat4 translasi = mat4(
+    1.0, 0.0, 0.0, trans.x,
+    0.0, 1.0, 0.0, trans.y,
+    0.0, 0.0, 1.0, trans.z,
+    0.0, 0.0, 0.0, 1.0
+  );
+
+  mat4 skalasi = mat4(
+    0.3, 0.0, 0.0, 0.0,
+    0.0, 0.3, 0.0, 0.0,
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
-  gl_Position = vec4(vPosition, 0.0, 1.0) * rotasi;
 
+  gl_Position = vec4(vPosition, 0.0, 1.0) * skalasi * ry;
+  gl_Position *=  translasi;
 }
-
-
